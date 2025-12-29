933525477299a735a33eb857025d47473076f1b1
Alex Hill <achill113@gmail.com>
2025-12-29 15:42:03 -0500

    feat: finished chapters 1 and 2

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	connectionString := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(connectionString)
+	if err != nil {
+		log.Fatalf("Failed to connect to RabbitMQ: %v", err)
+		return
+	}
+	defer conn.Close()
+
+	fmt.Println("Successfully connected to RabbitMQ")
+
+	// Wait for a signal to exit
+	sigChan := make(chan os.Signal, 1)
+
+	signal.Notify(sigChan, os.Interrupt)
+	<-sigChan
+
+	fmt.Println("Shutting down gracefully...")
 }
